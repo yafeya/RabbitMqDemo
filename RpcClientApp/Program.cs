c966b81113737f22e1983fbe6e4760a02f53d5cd
Feiyang_Yang <Feiyang_Yang@dell.com>
2019-03-08 17:48:27 +0800

    rpc demo

---
+++ RpcClientApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RpcClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rpcClient = new RpcClient();
+
+            var seed = new Random().Next(1, 30);
+            Console.WriteLine($" [x] Requesting fib({seed})");
+            var response = rpcClient.Call(seed.ToString());
+
+            Console.WriteLine(" [.] Got '{0}'", response);
+            rpcClient.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
